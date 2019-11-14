ce9944fa3133e4614b3f5b0b6c90dc3c7e3261f0
Becki Blumer <rblumer13@gmail.com>
2019-11-13 18:37:34 -0600

    4. What does this program do? / What is the runtime? (updated)

--- 4WhatDoesThisProgramDo.js
+++ 4WhatDoesThisProgramDo.js
@@ -6,3 +6,7 @@ function tree(t){
 }
 
 //This is a recursive function that can be applied to a binary tree to find the sum of all values in the tree.
+
+//The best case would be that no tree exists so returns 0 and runtime is O(1).
+//The average case is a balanced tree which runs the functions the same number of times for the right and left branches from the root, which would give us O(n)
+//The worst case is a skewed tree which would require more recursions on one side of the tree, so it would end up as O(log(n)) runtime
